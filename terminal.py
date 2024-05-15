e41be09b90dc4d0d69c786d0eff23df4fe3a5129
Naufal Rahfi Anugerah <156213717+rahfianugerah@users.noreply.github.com>
2024-05-15 22:21:59 +0700

    Add files via upload

---
+++ terminal.py
@@ -0,0 +1,15 @@
+import os
+import colorama
+
+from colorama import Fore,Style
+
+colorama.init(autoreset=True)
+w = Fore.WHITE+Style.BRIGHT
+g = Fore.GREEN+Style.BRIGHT
+os.system('cls')
+while True:
+    user = input(f"{w}"+"[user@user]~$ "+f"{g}")
+    if user == 'clear':
+        os.system('cls')
+    elif user == 'exit':
+        exit()
