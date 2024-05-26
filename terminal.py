9174170800f6b1307379c82c2cac3fe2383adccf
Naufal Rahfi Anugerah <156213717+rahfianugerah@users.noreply.github.com>
2024-05-26 22:42:39 +0700

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -1,15 +1,17 @@
-import os
-import colorama
+import os # Import os
+import colorama # Import colorama
 
-from colorama import Fore,Style
+from colorama import Fore, Style
 
 colorama.init(autoreset=True)
 w = Fore.WHITE+Style.BRIGHT
 g = Fore.GREEN+Style.BRIGHT
-os.system('cls')
+
+os.system('cls') # Clear out terminal
+
 while True:
     user = input(f"{w}"+"[user@user]~$ "+f"{g}")
-    if user == 'clear':
+    if user == 'clear': # Clear out terminal
         os.system('cls')
     elif user == 'exit':
-        exit()
+        exit() # Exit progream
