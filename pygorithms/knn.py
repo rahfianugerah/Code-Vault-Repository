1fd89496963f1df6c14632c756fb5c0d7edf2b85
Naufal Rahfi Anugerah <156213717+rahfianugerah@users.noreply.github.com>
2024-06-18 00:15:56 +0700

    Update knn.py

--- pygorithms/knn.py
+++ pygorithms/knn.py
@@ -1,5 +1,21 @@
 from sklearn.neighbors import KNeighborsClassifier
 
-knn = KNeighborsClassifier()
+# Model fitting and prediction for KNN
+knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(x_train, y_train)
+ypred_train_knn = knn.predict(x_train)
 ypred_test_knn = knn.predict(x_test)
+
+# Calculating metrics for Random Forest
+accuracy_knn, f1_knn, precision_knn, recall_knn = calculate_metrics(y_test, ypred_test_knn)
+
+# Storing the results in a DataFrame
+model_results = pd.DataFrame({
+    'KNN Accuracy': [accuracy_knn],
+    'KNN F1': [f1_knn],
+    'KNN Precision': [precision_knn],
+    'KNN Recall': [recall_knn]
+}, index=['Test'])
+
+# Display the results
+model_results.head()
