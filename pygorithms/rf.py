dfeae209597a26200a1a16b9eb1d787bda8c2bef
Naufal Rahfi Anugerah <156213717+rahfianugerah@users.noreply.github.com>
2024-06-19 13:56:43 +0700

    Update rf.py

--- pygorithms/rf.py
+++ pygorithms/rf.py
@@ -1,7 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 
 # Model fitting and prediction for Random Forest
-rf = RandomForestClassifier(n_estimators=10, random_state=42)
+rf = RandomForestClassifier(n_estimators=10, random_state=42) # n_estimators default values is 100 or 10
 rf.fit(x_train, y_train)
 ypred_train_rf = rf.predict(x_train)
 ypred_test_rf = rf.predict(x_test)
